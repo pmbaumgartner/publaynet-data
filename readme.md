1e89739567930ee076d8b7ad74cf7bc339944121
Peter Baumgartner <5107405+pmbaumgartner@users.noreply.github.com>
2022-12-27 12:23:52 -0500

    archive and statistics complete!

---
+++ readme.md
@@ -0,0 +1,117 @@
+# PubLayNet - Full PDF Subset Download Pipeline
+
+This repository is a data processing pipeline intended to download and extract full PDFs sourced from [PubLayNet](https://github.com/ibm-aur-nlp/PubLayNet). 
+
+The steps are all documented in the `Taskfile.yml` file, which uses Taskfile. This means you can replicate this workflow, but you'll need to install [Taskfile](https://taskfile.dev/installation/) first. If you don't want to do that, you can copy/paste the commands in the file into your own terminal, but none of the smart features of taskfile (e.g. checking if inputs have changed) will be available.
+
+## Steps
+
+1. We download the full set of PDFs from PubLayNet and extract them.
+2. We validate that any article is unique to the given train/dev/test splits. (It should be, they split it by journal)
+3. The original PDF files are single pages from an article, in the format `<PMID_PAGE>.pdf`. We merge the single page PDF files from an article into one PDF per article. 
+4. Once PDFs are merged, we identify potentially "complete" article PDFs: defined as PDFs that are more than a single page, and consist of pages from the minimum page number (0) to the maximum page number.
+   1. There are **`9131`** of these.
+   2. For more details, see [`merging_nodes.md`](merging_notes.md).
+5. We make requests to the PubMed Open Access API to find the URL on their FTP to download the full versions of the _complete_ pdfs.
+   1. We made successful requests for **`9124`** articles
+6. Given the article file URL, we download the article's `.tar.gz` archive, which contains the article's PDF and other supplementary files.
+   1. We successfully downloaded **`9124`** article archives.
+7. We extract the archive files then remove any non PDF files from those subfolders.
+   1. This results in **`8681`** folders with a single PDF.
+8. We celebrate with our **`8681`** full PDFs! 🎉
+
+
+## Statistics
+_This comes from the ` python scripts/calculate_statistics.py` file._
+
+```
+Total Folders: 9827
+Folders with single PDF: 8681
+  N PDFs per Folder   
+┏━━━━━━━━┳━━━━━━━━━━━┓
+┃        ┃ Frequency ┃
+┃ N PDFs ┃ (# PMIDs) ┃
+┡━━━━━━━━╇━━━━━━━━━━━┩
+│ 1      │      8681 │
+│ 2      │       343 │
+│ 3      │        49 │
+│ 4      │        16 │
+│ 7      │        10 │
+│ 5      │        10 │
+│ 6      │         6 │
+│ 10     │         2 │
+│ 8      │         2 │
+│ 21     │         1 │
+│ 14     │         1 │
+│ 13     │         1 │
+│ 9      │         1 │
+└────────┴───────────┘
+
+Page Count Statistics 
+┏━━━━━━━━━━━┳━━━━━━━━┓
+┃ Statistic ┃  Value ┃
+┡━━━━━━━━━━━╇━━━━━━━━┩
+│ count     │ 8681.0 │
+│ mean      │   7.31 │
+│ std       │   3.68 │
+│ min       │    2.0 │
+│ 25%       │    5.0 │
+│ 50%       │    7.0 │
+│ 75%       │    9.0 │
+│ max       │   79.0 │
+└───────────┴────────┘
+
+    Page Differences between    
+ PubLaynet and PubMed Download  
+┏━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━━┓
+┃ PMID - PDF ┃ Page Difference ┃
+┡━━━━━━━━━━━━╇━━━━━━━━━━━━━━━━━┩
+│ count      │          8681.0 │
+│ mean       │             3.3 │
+│ std        │             2.7 │
+│ min        │            -4.0 │
+│ 25%        │             2.0 │
+│ 50%        │             2.0 │
+│ 75%        │             4.0 │
+│ max        │            75.0 │
+└────────────┴─────────────────┘
+
+ Page Differences between PubLaynet 
+        and PubMed Download         
+       (as % of PubMed Pages)       
+┏━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━━━━━━┓
+┃ PMID - PDF ┃ Page Difference (%) ┃
+┡━━━━━━━━━━━━╇━━━━━━━━━━━━━━━━━━━━━┩
+│ count      │              8681.0 │
+│ mean       │                56.9 │
+│ std        │                19.4 │
+│ min        │                 5.1 │
+│ 25%        │                42.9 │
+│ 50%        │                60.0 │
+│ 75%        │                71.4 │
+│ max        │               157.1 │
+└────────────┴─────────────────────┘
+
+            Articles with Fewer Pages in PubMed Download            
+┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━━━━━━━┓
+┃                                           ┃      Page Difference ┃
+┃ PMID - PDF                                ┃ (PubMed - PubLayNet) ┃
+┡━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╇━━━━━━━━━━━━━━━━━━━━━━┩
+│ PMC5398342 - ckw142.pdf                   │           -2 (5 - 7) │
+│ PMC4794278 - cddiscovery201526.pdf        │          -2 (9 - 11) │
+│ PMC4868398 - 10.1177_2333393614565183.pdf │          -3 (9 - 12) │
+│ PMC4639696 - main.pdf                     │         -2 (11 - 13) │
+│ PMC5804988 - S2059866117002941a.pdf       │           -1 (2 - 3) │
+│ PMC6027614 - 41205_2016_Article_5.pdf     │           -1 (6 - 7) │
+│ PMC5833946 - cix705.pdf                   │           -1 (3 - 4) │
+│ PMC4678587 - JACE-98-3047.pdf             │           -2 (7 - 9) │
+│ PMC4760696 - dvv008.pdf                   │          -3 (8 - 11) │
+│ PMC5846682 - npjscilearn201615.pdf        │          -4 (7 - 11) │
+│ PMC4380276 - mic-01-267.pdf               │           -1 (3 - 4) │
+│ PMC4740918 - AJLM-2-31.pdf                │           -1 (7 - 8) │
+│ PMC4811365 - vev005.pdf                   │         -4 (10 - 14) │
+│ PMC4793549 - 40814_2015_Article_22.pdf    │          -2 (8 - 10) │
+│ PMC5176077 - archdischild-2016-310875.pdf │           -1 (4 - 5) │
+│ PMC5256467 - 41235_2016_Article_25.pdf    │         -2 (13 - 15) │
+└───────────────────────────────────────────┴──────────────────────┘
+```
---
+++ scripts/compress_articles.py
@@ -0,0 +1,159 @@
+import tarfile
+from pathlib import Path
+from shutil import copy
+from tempfile import TemporaryDirectory
+from typing import Optional, Tuple
+
+import pandas as pd
+import pypdfium2 as pdfium
+import typer
+from rich import print
+from rich.console import Console
+from rich.table import Table
+from tqdm import tqdm
+
+
+def _series_to_table(
+    series: pd.Series,
+    title: Optional[str] = None,
+    columns: Optional[Tuple[str, str]] = None,
+) -> Table:
+    if title is None:
+        title = series.name
+    table = Table(title=title)
+
+    if columns is None:
+        key_name, value_name = "Key", "Value"
+    else:
+        key_name, value_name = columns
+    table.add_column(key_name, justify="left", style="bold")
+    table.add_column(value_name, justify="right", style="#F5DF4D")
+    for index, value in series.items():
+        table.add_row(str(index), str(value))
+
+    return table
+
+
+def main(
+    source_dir: Path = typer.Argument(
+        ..., help="Directory containing subdirectories of PubMed PDFs"
+    ),
+    output_file: Path = typer.Argument(
+        ..., help="File to output PDFs from directories with a single file. (.tar.gz)"
+    ),
+    verbose: bool = typer.Option(
+        False,
+        "--verbose",
+        "-v",
+        help="Verbose mode - prints out statistics on the document collection.",
+    ),
+    merge_data_file: Optional[Path] = typer.Option(
+        None,
+        help="Path to the merged metdata file (.csv), for printing out additional statistics.",
+    ),
+):
+    console = Console()
+
+    pdf_paths = list(source_dir.glob("**/*.pdf"))
+
+    pdf_data = []
+    for path in pdf_paths:
+        pdf_data.append(
+            {"name": path.name, "PMID": path.parent.stem, "size": path.stat().st_size}
+        )
+
+    df = pd.DataFrame(pdf_data)
+
+    pdfs_per_id = df.groupby("PMID").size().sort_values(ascending=False)
+
+    single_pdfs = list(pdfs_per_id[pdfs_per_id == 1].index)
+    if verbose:
+        print(f"Total Folders: {len(df)}")
+        print(f"Folders with single PDF: {len(single_pdfs)}")
+        print(
+            _series_to_table(
+                pdfs_per_id.value_counts(),
+                "N PDFs per Folder",
+                ("N PDFs", "Frequency\n(# PMIDs)"),
+            )
+        )
+
+    df_singles = df[df["PMID"].isin(single_pdfs)].copy()
+    paths_for_single_pdfs = [p for p in pdf_paths if p.parent.stem in single_pdfs]
+
+    if verbose:
+        page_counts = {}
+        for path in tqdm(paths_for_single_pdfs, desc="Counting Pages per PDF"):
+            doc = pdfium.FPDF_LoadDocument(str(path), None)
+            page_count = pdfium.FPDF_GetPageCount(doc)
+            page_counts[path.parent.stem] = page_count
+
+        df_singles["page_count"] = df_singles["PMID"].map(page_counts)
+
+        assert df_singles["page_count"].isnull().sum() == 0
+        print(
+            _series_to_table(
+                df_singles["page_count"].describe().round(2),
+                "Page Count Statistics",
+                ("Statistic", "Value"),
+            )
+        )
+    if merge_data_file is not None and merge_data_file.exists():
+        merge_data = pd.read_csv(merge_data_file, index_col=0)
+        complete_data = df_singles.merge(merge_data, on=["PMID"]).assign(
+            page_difference=lambda d: (d["page_count"] - d["n_pages"]).astype(int),
+            original_pct=lambda d: (d["n_pages"] / d["page_count"]) * 100,
+        )
+
+        print(
+            _series_to_table(
+                complete_data["page_difference"].describe().round(1),
+                "Page Differences between PubLaynet and PubMed Download",
+                ("PMID - PDF", "Page Difference"),
+            )
+        )
+
+        print(
+            _series_to_table(
+                complete_data["original_pct"].describe().round(1),
+                "Page Differences between PubLaynet and PubMed Download\n(as % of PubMed Pages)",
+                ("PMID - PDF", "Page Difference (%)"),
+            )
+        )
+
+        fewer_pages_in_download = complete_data[complete_data["page_difference"] < 0]
+
+        # I hate formatting these stings.
+        fewer_pages_series = fewer_pages_in_download.assign(
+            pmid_pdf_name=lambda d: d["PMID"] + " - " + d["name"],
+            page_difference_str=lambda d: d["page_difference"].astype(str)
+            + " ("
+            + d["page_count"].astype(str)
+            + " - "
+            + d["n_pages"].astype(int).astype(str)
+            + ")",
+        ).set_index("pmid_pdf_name")["page_difference_str"]
+
+        print(
+            _series_to_table(
+                fewer_pages_series,
+                "Articles with Fewer Pages in PubMed Download",
+                ("PMID - PDF", "Page Difference\n(PubMed - PubLayNet)"),
+            )
+        )
+    with console.status("Collecting and Compressing PDFs"):
+        with TemporaryDirectory() as archive_dir:
+            archive_dir = Path(archive_dir)
+            for path in paths_for_single_pdfs:
+                pmid = path.parent.stem
+                copy(path, archive_dir / f"{pmid}.pdf")
+
+            with tarfile.open(output_file, "w:gz") as tar:
+                tar.add(archive_dir, arcname=".")
+    print(
+        f"✔️ Compressed {len(paths_for_single_pdfs)} PDFs into [green]{output_file}[/green]."
+    )
+
+
+if __name__ == "__main__":
+    typer.run(main)
